cff25c1d888e79acc7f68d781bb4df26eb8eb36c
oluwatobiloba <oluwateezzy03@gmail.com>
2023-08-31 19:55:21 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,15 +15,18 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
+        """ format """
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str :
     """ filter datum"""
     return re.sub(r'(?P<field>{})=[^{}]*'.format('|'.join(fields), separator),
                    r'\g<field>={}'.format(redaction), message)
-    
