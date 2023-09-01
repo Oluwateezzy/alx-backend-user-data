92087a62ccb6e635eb4d89646228b4c3cf09e58f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-01 11:18:59 +0100

    update

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bcrypt.hashpw:
+    """ password hashing """
+    bytye = password.encode("utf-8")
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(bytye, salt)
