e51d371590fd431edc98c405a6bfcc23b25ae49e
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-01 11:29:21 +0100

    update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,6 +11,7 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(bytye, salt)
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ check validity """
     return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
