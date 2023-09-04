7882d01d0eef963629d1c7ee74efe2555ca43a4f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-04 17:41:03 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,3 +14,18 @@ class BasicAuth(Auth):
         if authorization_header is None or type(authorization_header) != str or not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(' ')[1]
+    
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ decode base64 """
+        if base64_authorization_header is None or type(base64_authorization_header) != str:
+            return None
+        try:
+            decoded_byte = base64.b64decode(base64_authorization_header)
+            decoded_str  = decoded_byte.decode('utf-8')
+            return decoded_str
+        except:
+            return None
+    
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ extract user credentials """
+        
