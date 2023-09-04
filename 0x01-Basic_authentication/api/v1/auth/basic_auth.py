1f467eb0edca14f895202f746aa6b1aecdc7d8b0
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-04 18:17:55 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 from flask import request
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -28,4 +30,23 @@ class BasicAuth(Auth):
     
     def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
         """ extract user credentials """
-        
+        if decoded_base64_authorization_header is None or type(decoded_base64_authorization_header) != str or ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, pwd = decoded_base64_authorization_header.split(':')
+        return user_email, pwd
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ user Object """
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+        try:
+            user_instance = User()
+            users = user_instance.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+            return None
