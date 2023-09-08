b9fca45ffca7bcc794d54a89467085424ddee402
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-08 11:04:21 +0100

    update

---
+++ 0x02-Session_authentication/models/main_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Check response
+"""
+
+if __name__ == "__main__":
+    from api.v1.auth.auth import Auth
+
+    a = Auth()
+    res = a.require_auth("/api/v1/status", ["/api/v1/stats/", "/api/v1/status/", "/api/v1/users/"])
+    if res:
+        print("require_auth must return False when path is in excluded_paths - slash tolerant")
+        exit(1)
+    print("OK", end="")
