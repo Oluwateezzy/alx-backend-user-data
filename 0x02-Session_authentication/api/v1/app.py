45bb39139b85678ae4e62dd322bea1781630feaa
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-08 12:09:51 +0100

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -31,9 +31,9 @@ def beforeRequest() -> None:
         if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']):
             if auth.authorization_header(request) is None:
                 abort(401)
-            request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
                 abort(403)
+            request.current_user = auth.current_user(request)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x02-Session_authentication/main_0.py
+++ 0x02-Session_authentication/main_0.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 """ Main 0
 """
-from api.v1.auth.auth import Auth
+import base64
+from api.v1.auth.basic_auth import BasicAuth
+from models.user import User
 
-a = Auth()
+""" Create a user test """
+user_email = "bob@hbtn.io"
+user_clear_pwd = "H0lbertonSchool98!"
 
-excluded_paths = ["/api/v1/stat*"]
-print(a.require_auth("/api/v1/stats", excluded_paths))
+user = User()
+user.email = user_email
+user.password = user_clear_pwd
+print("New user: {}".format(user.id))
+user.save()
+
+basic_clear = "{}:{}".format(user_email, user_clear_pwd)
+print("Basic Base64: {}".format(base64.b64encode(basic_clear.encode('utf-8')).decode("utf-8")))
