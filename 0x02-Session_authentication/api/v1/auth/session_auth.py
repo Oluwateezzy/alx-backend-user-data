4517b22d5967df6448a2e02ef078bd941c7be372
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-08 12:38:07 +0100

    update

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,19 @@
 """
 from flask import request
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Session Authentication """
+    user_id_by_session_id = {}
     
+    def create_session(self, user_id: str = None) -> str:
+        """ create session """
+        if user_id is None:
+            return None
+        if type(user_id) != str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
