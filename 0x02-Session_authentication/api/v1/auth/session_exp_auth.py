3bab03a25f29f17075a2b75ae54109ba79aa3637
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-12 15:54:26 +0100

    update

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Basic Auth class
+"""
+from api.v1.auth.session_auth import SessionAuth
+from os import getenv
+from datetime import datetime
+
+
+class SessionExpAuth(SessionAuth):
+    """ session expiration """
+    def __init__(self) -> None:
+        env = getenv("SESSION_DURATION")
+        self.session_duration = int(env) if env else 0
+
+    def create_session(self, user_id=None):
+        """ create session """
+        id = super().create_session(user_id)
+        if not id:
+            return None
+        self.user_id_by_session_id[id] = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+        return id
+
+    def user_id_for_session_id(self, session_id=None):
+        """ user id for session """
+        if session_id is None:
+            return None
+        if session_id not in self.user_id_by_session_id.keys():
+            return None
+        session_dict = self.user_id_by_session_id[session_id]
+        if self.session_duration <= 0:
+            return session_dict["user_id"]
+        if not session_dict["created_at"]:
+            return None
+        created_at = session_dict["created_at"].timestamp()
+        if created_at + self.session_duration < datetime.now().timestamp():
+            return None
+        return session_dict["user_id"]
