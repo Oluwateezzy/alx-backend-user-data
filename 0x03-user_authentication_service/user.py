929f3859008423dc33821933410be77b786a5f25
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-13 15:56:17 +0100

    update

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,9 +9,9 @@ Base = declarative_base()
 class User(Base):
     """ User Models """
     __tablename = 'users'
-    
+
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
