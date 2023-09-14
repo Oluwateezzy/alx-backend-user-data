deffb4bccaa7a7ef6bfbafaa3978b4dd7c1a20d2
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-14 16:35:40 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,4 +91,17 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+    
+    def update_password(
+        self, reset_token: str, password: str
+    ) -> None:
+        """ update password """
+        if not reset_token and not password:
+            return None
         
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        hash = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=hash, reset_token=None)
