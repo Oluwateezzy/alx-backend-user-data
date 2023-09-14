b489357cffe6411fb2aff5ea90301b343cc6981c
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-14 16:15:06 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """ Authentication
 """
 
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from uuid import uuid4
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
@@ -39,4 +39,14 @@ class Auth:
             user = self._db.add_user(email, hash)
             return user
 
-    def valid_login(self, email: str, password: str) -> bool
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validate login """
+        try:
+            user = self._db.find_user_by(email=email)
+            return checkpw(
+                password.encode('utf-8'), user.hashed_password
+            )
+        except NoResultFound:
+            return False
+    
+    def 
