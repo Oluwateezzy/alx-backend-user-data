0ef33a6cf1bb42f7222a515dad6ffd16e6143ddc
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-14 16:04:09 +0100

    update

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" Authentication
+"""
+
+from bcrypt import hashpw, gensalt
+from uuid import uuid4
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+from db import DB
+from user import User
+
+
+def _hash_password(password: str) -> str:
+    """ hash password """
+    return hashpw(password.encode('utf-8'), gensalt())
+
+
+def _generate_uuid() -> str:
+    """ generate uuid """
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """ initialize """
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str) -> User:
+        """ Register User """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists")
+        except NoResultFound:
+            hash = _hash_password(password)
+            user = self._db.add_user(email, hash)
+            return user
+    
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -38,3 +40,25 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """ Find user """
+        try:
+            record = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if record is None:
+            raise NoResultFound
+        return record
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Update user """
+        user_record = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if hasattr(user_record, key):
+                setattr(user_record, key, value)
+            else:
+                raise ValueError
+        self._session.commit()
+        return None
+    
