4d94b52cac5885e6db0e25821a94f7aef3eacddf
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-15 03:39:33 +0100

    uppate

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,7 +78,7 @@ class Auth:
             return None
         try:
             user = self._db.find_user_by(id=user_id)
-            self._db.update_user(user, session_id=None)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return None
 
